d271ce384b35e1d3afad54e3148d2cd0bbc5f167
Aftaza <taftazani17@student.ub.ac.id>
2023-10-01 19:38:12 +0700

    Try reduce opacity image

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import cv2
+import os
+from PIL import Image, ImageEnhance
 from cvzone.HandTrackingModule import HandDetector
+
+def  reduceOpc(im, opacity):
+    """
+    Return an image with reduce opacity
+    args:
+        im: Image to reduce must Pillow read
+        opacity: alpha number
+    """
+    assert opacity >= 0 and opacity <= 1
+    if im.mode != 'RGBA':
+        im = im.convert('RGBA')
+    else:
+        im = im.copy()
+    
+    alpha = im.split()[3]
+    alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
+    im.putalpha(alpha)
+    return im
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
@@ -7,9 +27,17 @@ cap.set(4, 720)
 
 detector = HandDetector(detectionCon=0.65)
 
+dirPath = './img/pieces'
+pieceImg = os.listdir(dirPath)
+
 img1 = cv2.imread('img/pieces/piece_0.png')
 img1 = cv2.resize(img1, (150, 150))
 ox, oy = 200, 200
+
+# img = Image.open("img/pieces/piece_0.png")
+# img.show()
+# img = reduceOpc(img, 0.8)
+# img.show()
 
 while True:
     success, img = cap.read()
